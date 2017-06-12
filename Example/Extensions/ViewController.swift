6ff3a94e63570a9e8362fc4efe3407ca66216bd1
Daniel Zanchi <danny.zanchi@gmail.com>
2017-06-12 17:00:18 +0200

    Initial commit

---
+++ Example/Extensions/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Extensions
+//
+//  Created by Danny182 on 06/12/2017.
+//  Copyright (c) 2017 Danny182. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
