183eab8067965998c5e1a4841873ab733c2b2534
Daniel Zanchi <danny.zanchi@gmail.com>
2017-06-12 17:29:43 +0200

    localisedprice for SKProduct()

---
+++ Extensions/Classes/SKProduct+LocalizedPrice.swift
@@ -0,0 +1,20 @@
+//
+//  SKProduct+LocalizedPrice.swift
+//  Pods
+//
+//  Created by Daniel Zanchi on 12/06/2017.
+//
+//
+
+import StoreKit
+
+extension SKProduct {
+    
+    // returns the price of a SKProduct ex: "€1.09"
+    public func localizedPrice() -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = self.priceLocale
+        return formatter.string(from: self.price)!
+    }
+}
