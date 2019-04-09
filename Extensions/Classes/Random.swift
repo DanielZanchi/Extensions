e182f80e5bb70788c8ce95df1d81ac0873d72142
Daniel Zanchi <danny.zanchi@gmail.com>
2019-04-09 16:52:21 +0200

    public static needs

--- Extensions/Classes/Random.swift
+++ Extensions/Classes/Random.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 extension Int {
-    static func random(min: Int, max: Int) -> Int {
+    static public func random(min: Int, max: Int) -> Int {
         return min + Int(arc4random_uniform(UInt32(max - min + 1)))
     }
 }
