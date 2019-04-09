32d57f59797278c8e173e9561d6a0a6e16ac2c03
Daniel Zanchi <danny.zanchi@gmail.com>
2019-04-09 12:56:39 +0200

    completely removed cgfloat random

--- Extensions/Classes/Random.swift
+++ Extensions/Classes/Random.swift
@@ -8,15 +8,6 @@
 
 import Foundation
 
-import CoreGraphics
-
-extension CGFloat {
-    
-    static func random(min: CGFloat, max: CGFloat) -> CGFloat {
-        return CGFloat.random() * (max - min) + min
-    }
-}
-
 extension Int {
     static func random(min: Int, max: Int) -> Int {
         return min + Int(arc4random_uniform(UInt32(max - min + 1)))
